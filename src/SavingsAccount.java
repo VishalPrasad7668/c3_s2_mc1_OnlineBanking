f3f0643a12a4c4e70dead7d60adb98a321f2398c
VISHAL PRASAD <vishalprasad1117@gmail.com>
2022-08-11 16:37:49 +0530

    implemented SavingsAccount class and adding attributes to it

---
+++ src/SavingsAccount.java
@@ -0,0 +1,10 @@
+public class SavingsAccount {
+    long accountNumber;
+    String accountHolderName;
+    double accountBalance;
+    String accountStatus;
+
+    SavingsAccount(long accountNumber, String accountHolderName, double accountBalance, String accountStatus) {
+        
+    }
+}
