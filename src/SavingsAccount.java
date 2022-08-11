7546a3793cbdcb63ff30c68bbdc0d82ae63ec2da
VISHAL PRASAD <vishalprasad1117@gmail.com>
2022-08-11 17:33:36 +0530

    implemented main class

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,10 +1,32 @@
+import java.util.Random;
+
 public class SavingsAccount {
     long accountNumber;
     String accountHolderName;
     double accountBalance;
     String accountStatus;
 
-    SavingsAccount(long accountNumber, String accountHolderName, double accountBalance, String accountStatus) {
-        
+    SavingsAccount(String accountHolderName, double accountBalance, String accountStatus) {
+        this.accountNumber = generateAccountNumber();
+        this.accountHolderName = accountHolderName;
+        this.accountBalance = accountBalance;
+        this.accountStatus = accountStatus;
+
+    }
+
+    public long generateAccountNumber() {
+        long branchCode = 4907L;
+        Random random = new Random();
+        accountNumber = random.nextLong(4907000000000L, 4907500000000L);
+        return accountNumber;
+
+    }
+
+    public double debitCash(double amount) {
+        return accountBalance - amount;
+    }
+
+    public double creditCash(double amount) {
+        return accountBalance + amount;
     }
 }
---
+++ src/SavingsAccountImpl.java
@@ -0,0 +1,7 @@
+public class SavingsAccountImpl {
+    public static void main(String[] args) {
+        SavingsAccount account = new SavingsAccount("Ramesh", 5000.0, "active");
+
+
+    }
+}
